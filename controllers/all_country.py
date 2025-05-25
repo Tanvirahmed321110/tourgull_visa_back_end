ebff2a2e445f06ee431ef0d2ff40a3daf1be45f7
tanvir321110 <tanvirahmed321110@gmail.com>
2025-05-25 19:37:46 +0600

    update home page

--- controllers/all_country.py
+++ controllers/all_country.py
@@ -8,10 +8,12 @@ class allCountry(http.Controller):
         continent = req.env['tourgull_visa.continent'].search([])
         categories = req.env['tourgull_visa.category'].search([])
         suggest_slider = req.env['tourgull_visa.suggest_service_slider'].search([])
+        country = req.env['tourgull_visa.country'].search([])
 
         return req.render('tourgull_visa.all_country',{
             'all_country_data':all_country_data,
             'continent':continent,
             'categories':categories,
-            'suggest_slider':suggest_slider
+            'suggest_slider':suggest_slider,
+            'country' : country
         })
